5153d5071cdda70060fea6d624f0b36ab531a37d
kjy93k <46341006+kjy93k@users.noreply.github.com>
2019-01-17 16:48:01 +0900

    20190117

---
+++ layout01/sub_03.php
@@ -0,0 +1,7 @@
+<?include('./include/header.php');?>
+		<div class="container clearfix">
+			<section class="content">content</section>
+			<aside class="aside">aside</aside>
+		</div>
+<?include('./include/footer.php');?>
+
